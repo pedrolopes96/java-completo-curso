1203ad28d59992c9acc5020eb1949f82659c6ce1
Pedro <pedroflopes1710@gmail.com>
2023-05-24 16:44:19 -0300

    Ex002

---
+++ Ex002.java
@@ -0,0 +1,12 @@
+public class Ex002 {
+
+    public static void main(String[] args) {
+        double a;
+        int b;
+
+        a = 5.0;
+        b = (int) a;
+
+        System.out.println(b);
+    }
+}
