05a8e4bb9d627853b35d88c344c3d63c7fa6679c
Pedro <pedroflopes1710@gmail.com>
2023-05-24 16:45:57 -0300

    Ex002

--- Ex002.java
+++ Ex002.java
@@ -1,12 +1,17 @@
 public class Ex002 {
 
     public static void main(String[] args) {
+        // Declaração de variáveis
         double a;
         int b;
 
+        // Atribuição de valores
         a = 5.0;
+        
+        // Conversão de tipos
         b = (int) a;
 
+        // Impressão de valores
         System.out.println(b);
     }
 }
