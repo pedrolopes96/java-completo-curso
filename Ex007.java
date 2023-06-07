4e5558068852cce71bac3280c5512627703c978c
Pedro Fernandes Lopes <pedroflopes1710@gmail.com>
2023-06-07 15:01:34 -0300

    Ex007

--- Ex007.java
+++ Ex007.java
@@ -2,11 +2,13 @@
 //Exercício 07: Crie um programa que calcule a raiz quadrada, potenciação e arredondamento de um número.
 public class Ex007 {
     public static void main(String[]Args){
+        //Declaração de variáveis
         double x = 3.0;
         double y = 4.0;
         double z = -5.0;
         double A, B, C;
 
+        //Raiz quadrada
         A = Math.sqrt(x);
         B = Math.sqrt(y);
         C = Math.sqrt(25.0);
@@ -14,6 +16,7 @@ public class Ex007 {
         System.out.println("Raiz quadrada de " + y + " = " + B);
         System.out.println("Raiz quadrada de 25 = " + C);
 
+        //Potenciação
         A = Math.pow(x, y);
         B = Math.pow(x, 2.0);
         C = Math.pow(5.0, 2.0);
@@ -21,6 +24,7 @@ public class Ex007 {
         System.out.println(x + " elevado ao quadrado = " + B);
         System.out.println("5 elevado ao quadrado = " + C);
 
+        //Arredondamento
         A = Math.abs(y);
         B = Math.abs(z);
         System.out.println("Valor absoluto de " + y + " = " + A);
