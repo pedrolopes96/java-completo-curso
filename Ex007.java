fdc7a035a97ac6bc60abedf5a45576194b529711
Pedro Fernandes Lopes <pedroflopes1710@gmail.com>
2023-06-22 02:06:38 -0300

    Ex007

--- Ex007.java
+++ Ex007.java
@@ -28,6 +28,7 @@ public class Ex007 {
         A = Math.abs(y);
         B = Math.abs(z);
         System.out.println("Valor absoluto de " + y + " = " + A);
+
         System.out.println("Valor absoluto de " + z + " = " + B);
     }
 }
